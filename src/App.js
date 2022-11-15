c2517627be46f6fa20e3ba8688ba75c2e6872c29
smh1988-2 <seamhurley1988@gmail.com>
2022-11-15 12:10:46 -0800

    added basic generator logic

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [output, setOutput] = useState([]);
+  const [length, setLength] = useState(5)
+
+  const words = ["word one", "word 2", "word 3", "word 4"];
+
+  function generateWords() {
+    let newArray =[]
+    for (let i = 0; i < length; i++) {
+      newArray = [...newArray, words[Math.floor(Math.random() * words.length)]]
+    }
+    setOutput(newArray);
+  }
+
   return (
     <div>
-     app goes here
+      <button onClick={generateWords}>Generate words</button>
+      {output.join(" ")}
     </div>
   );
 }
