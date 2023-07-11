ff5569c6e9d294c380441821a15b9b71166c8254
Sean Hurley <seanmhurley1988@gmail.com>
2023-07-10 22:51:40 -0700

    added GA

--- src/App.js
+++ src/App.js
@@ -3,13 +3,20 @@ import Header from "./components/Header";
 import Generator from "./components/Generator";
 import Output from "./components/Output";
 import Footer from "./components/Footer";
-
 import { useState } from "react";
+
+import ReactGA from "react-ga4";
+
+  
 
 function App() {
   const [output, setOutput] = useState([]);
   const [paragraphs, setParagraphs] = useState(false);
   const [copied, setCopied] = useState(false);
+
+  const TRACKING_ID = "G-YGC5RRJ8ZV"; // OUR_TRACKING_ID
+  ReactGA.initialize(TRACKING_ID);
+
 
   return (
     <div className="content-wrap">
