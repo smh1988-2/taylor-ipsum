a538cf70d950fb0a17f6d81251b7264df157bc33
Sean Hurley <seanmhurley1988@gmail.com>
2023-07-10 23:05:43 -0700

    added longer description

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,11 @@ function Header() {
   return (
     <header className="header-container">
       <h1>taylorem ipswift</h1>
-      <p className="header-text">Tired of using Lorem Ipsum for all your placeholder needs? Add some flair to your text with Taylor Swift-inspired words.</p>
+      <p className="header-text">Introducing the Taylor Swift Lorem Ipsum Generator! Get ready to inject a dose of Taylor Swift's lyrical magic into your design projects, websites, or any creative endeavor that needs some Swift-inspired placeholder text.</p>
+
+      <p className="header-text">With the Taylor Swift Lorem Ipsum Generator, you'll experience the perfect blend of Taylor's iconic lyrics and the classic placeholder text known as lorem ipsum. This fusion creates a unique and delightful tool that adds a touch of whimsy, emotion, and musicality to your projects.</p>
+
+      <p className="header-text">Not only does the Taylor Swift Lorem Ipsum Generator provide you with placeholder text, but it also adds a touch of creativity and inspiration to your work. So, whether you're a designer seeking an imaginative twist, a developer wanting to surprise and engage users, or simply a Taylor Swift enthusiast looking to incorporate her lyrical genius into your projects, the Taylor Swift Lorem Ipsum Generator is the perfect tool for you.</p>
     </header>
   );
 }
